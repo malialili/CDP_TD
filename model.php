9eb535fe31aa78a059fbe4e43c19d13f5b0d41ee
Yedir <yedir.ouarmassi@laposte.net>
2015-09-25 10:32:51 +0200

    ajout v1 model php

---
+++ model.php
@@ -0,0 +1,47 @@
+<?php
+class Atelier {
+ // Déclarations des données membres
+ var $_title;
+ var $_type;
+ var $_theme[];
+ var $_time;
+ var $_capacity;
+ var $_desc;
+ var $_animator;
+ var $_target[];
+ var $_com;
+ var $_addr;
+ var $_acces;
+ 
+ // Déclarations des méthodes
+ function add() {
+	$at= new Atelier;
+	$at ->_title=$title;
+	$at ->_type=$type;
+	$at ->_time=$time;
+	$at ->_capacity=$capacity;
+	$at ->_desc=$desc;
+	$at ->_animator=$animator;
+	$at ->_com=$com;
+	$at ->_acces=$acces;
+	$at ->_addr=$addr;
+	
+	$at ->_theme[]=$computScience;
+	$at ->_theme[]=$chemestry;
+	$at ->_theme[]=$history;
+	$at ->_theme[]=$othertheme;
+	
+	$at ->_public[]=$primary
+	$at ->_public[]=$class6
+	$at ->_public[]=$class5
+	$at ->_public[]=$class4
+	$at ->_public[]=$class3
+	$at ->_public[]=$class2
+	$at ->_public[]=$class1
+	$at ->_public[]=$endclass
+	$at ->_public[]=$prepa
+	$at ->_public[]=$university
+	
+	}
+}
+?>
